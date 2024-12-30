c432f4095bfd788ec4a7dfd0e8369de17917eb98
PhamNhuQuocBao <103192057+PhamNhuQuocBao@users.noreply.github.com>
2024-12-30 09:13:02 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,6 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0', // Listen on all network interfaces
-    port: process.env.PORT ? parseInt(process.env.PORT) : 3000, // Use $PORT or fallback to 3000
+    port: 3000
   },
 })
