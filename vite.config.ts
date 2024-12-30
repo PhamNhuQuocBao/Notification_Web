41e4836ce42f7e97aefffe80f07ce291756741d9
PhamNhuQuocBao <103192057+PhamNhuQuocBao@users.noreply.github.com>
2024-12-30 09:11:36 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Listen on all network interfaces
+    port: process.env.PORT ? parseInt(process.env.PORT) : 3000, // Use $PORT or fallback to 3000
+  },
 })
